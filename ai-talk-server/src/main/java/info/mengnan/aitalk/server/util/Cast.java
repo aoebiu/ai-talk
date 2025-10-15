d565e77e8ed5b70b8d6138eba59f73ef328b92eb
aoebiu <ltl1510@126.com>
2025-10-15 10:29:03 +0800

    refactor(util): 删除无用的类型转换工具类

--- ai-talk-server/src/main/java/info/mengnan/aitalk/server/util/Cast.java
+++
@@ -1,9 +0,0 @@
-package info.mengnan.aitalk.server.util;
-
-public class Cast {
-
-    @SuppressWarnings("unchecked")
-    public static <T> T cast(Object object) {
-        return (T) object;
-    }
-}
